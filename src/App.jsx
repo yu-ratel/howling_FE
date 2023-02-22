1f41ea4fbccba7c95cad0b2f199db0f1038b0279
Cos Kim <junzerokim@gmail.com>
2023-02-22 18:13:59 +0900

    Init

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { BrowserRouter, Routes } from 'react-router-dom';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>wef</Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
