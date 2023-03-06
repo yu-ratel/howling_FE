a75a475d268231c6cefc1e6fbbca901e76a1f550
seooh <ohsh1115@gmail.com>
2023-03-07 01:08:56 +0900

    Add:StartPage

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 import Profile from './Profile';
 import KakaoMap from './Map/KakaoMap';
+import Start from './Start';
 
 function App() {
+  const [page, pageSet] = useState(false);
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      pageSet(true);
+    }, 2500);
+  });
+
   return (
     <BrowserRouter>
-      <Profile />
-      <Routes>
-        <Route path="/" element={<KakaoMap />} />
-      </Routes>
+      {/* <Profile /> */}
+      <Start />
+      {/* <Routes>
+{page === true ? <Route path="/" element={<KakaoMap />} /> : <Route path="/" element={<Start />} />}
+      </Routes> */}
     </BrowserRouter>
   );
 }
---
+++ src/Start/index.jsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import { motion } from 'framer-motion';
+import styled from 'styled-components';
+
+import logoSrc from '../howling_img.png';
+
+function Start() {
+  return (
+    <Container>
+      <Wrapper>
+        <Logo
+          animate={{ y: 0, scale: 0.2 }}
+          initial={{ y: -100, scale: 0 }}
+          transition={{ type: 'spring', duration: 3, bounce: 0.3 }}
+          src={logoSrc}
+          alt="logo-img"
+        />
+        <Title>Howling</Title>
+      </Wrapper>
+    </Container>
+  );
+}
+
+export default Start;
+
+const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background-color: rgb(23, 22, 48);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Wrapper = styled.div`
+  width: 600;
+  height: 600;
+`;
+
+const Logo = styled(motion.img)`
+  img {
+    width: 200;
+    height: 200;
+  }
+`;
+
+const Title = styled.h1`
+  color: white;
+  font-size: 250%;
+  font-style: italic;
+  font-family: 'Times New Roman', Times, serif;
+`;
