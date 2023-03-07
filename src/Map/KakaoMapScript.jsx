7e022919ee4604365f7d5526e78df47a5f199b82
seooh <ohsh1115@gmail.com>
2023-03-07 22:59:32 +0900

    Add: page transition

--- src/Map/KakaoMapScript.jsx
+++ src/Map/KakaoMapScript.jsx
@@ -6,8 +6,6 @@ export default function KakaoMapScript() {
     center: new kakao.maps.LatLng(33.450701, 126.570667),
     level: 3,
     draggable: false,
-    draggable: false,
-    level: 2,
   };
   const map = new kakao.maps.Map(container, options);
 
