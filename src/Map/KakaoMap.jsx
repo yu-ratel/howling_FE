b8238744ecf8ba077dee61b1f8c46d4adcdd3b01
Cos Kim <junzerokim@gmail.com>
2023-03-11 01:18:59 +0900

    Add: KakaoMap mouseup event

--- src/Map/KakaoMap.jsx
+++ src/Map/KakaoMap.jsx
@@ -1,28 +1,16 @@
 import React, { useEffect } from 'react';
 import styled from 'styled-components';
 import KakaoMapScript from './KakaoMapScript';
-import * as zoomIn from './KakaoMapScript';
 
 export default function KakaoMap() {
   useEffect(() => {
     KakaoMapScript();
   }, []);
 
-  return (
-    <Map id="map">
-      <Box onClick={zoomIn} />
-    </Map>
-  );
+  return <Map id="map" />;
 }
 
 const Map = styled.div`
   width: 100vw;
   height: 100vh;
-`;
-
-const Box = styled.button`
-  z-index: 1000;
-  width: 100px;
-  height: 100px;
-  position: absolute;
 `;
--- src/Map/KakaoMapScript.jsx
+++ src/Map/KakaoMapScript.jsx
@@ -2,70 +2,50 @@ const { kakao } = window;
 
 export default function KakaoMapScript() {
   const container = document.getElementById('map');
+
+  // 지도를 표시할 div
   const options = {
-    center: new kakao.maps.LatLng(33.450701, 126.570667),
-    level: 3,
-    draggable: false,
+    center: new kakao.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표
+    level: 3, // 지도의 확대 레벨
   };
-  const map = new kakao.maps.Map(container, options);
 
-  function displayLevel() {
-    const levelEl = document.getElementById('maplevel');
-    levelEl.innerHTML = `현재 지도 레벨은  ${map.getLevel()} 레벨입니다.`;
-  }
+  const map = new kakao.maps.Map(container, options); // 지도를 생성
 
-  function zoomIn() {
-    // 현재 지도의 레벨을 얻어옵니다
-    const level = map.getLevel();
+  const marker = new kakao.maps.Marker({}); // 마커 생성
 
-    // 지도를 1레벨 내립니다 (지도가 확대됩니다)
-    map.setLevel(level - 1);
+  marker.setMap(map); // 마커 실행
 
-    // 지도 레벨을 표시합니다
-    displayLevel();
-  }
+  marker.setDraggable(true);
 
-  function zoomOut() {
-    // 현재 지도의 레벨을 얻어옵니다
-    const level = map.getLevel();
+  kakao.maps.event.addListener(map, 'mouseup', (mouseEvent) => {
+    // 클릭한 위치 경도 위도 가져옴
+    const latlng = mouseEvent.latLng;
 
-    // 지도를 1레벨 올립니다 (지도가 축소됩니다)
-    map.setLevel(level + 1);
+    marker.setPosition(latlng); // 마커 클릭한 위치로 옮김
 
-    // 지도 레벨을 표시합니다
-    displayLevel();
-  }
-  // const marker = new kakao.maps.Marker({
-  //   position: map.getCenter(),
-  // });
+    const message = `경도는 ${latlng.getLat()}이고, 위도는 ${latlng.getLng()}입니다.`;
+    console.log(message);
+  });
 
-  // marker.setMap(map);
-
-  // kakao.maps.event.addListener(map, 'click', (mouseEvent) => {
-  //   const latlng = mouseEvent.latLng;
-
-  //   marker.setPosition(latlng);
-
-  //   const message = `클릭한 위치의 위도는 ${latlng.getLat()} 이고, 경도는 ${latlng.getLng()} 입니다`;
-  // console.log(message);
-  // });
-
+  // 현재 위치 로직
   function displayMarker(locPosition, message) {
-    const marker = new kakao.maps.Marker({
+    // 마커 생성
+    const locationMarker = new kakao.maps.Marker({
       // eslint-disable-next-line object-shorthand
       map: map,
-      position: locPosition,
+      position: locPosition, // 위치는 현재 위치
     });
 
-    const iwContent = message;
-    const iwRemoveable = true;
+    const iwContent = message; // 인포윈도우 창 문구
+    const iwRemoveable = true; // 인포윈도우 창 닫기
 
+    // 인포윈도우 생성
     const infowindow = new kakao.maps.InfoWindow({
       content: iwContent,
       removable: iwRemoveable,
     });
 
-    infowindow.open(map, marker);
+    infowindow.open(map, locationMarker);
 
     map.setCenter(locPosition);
   }
@@ -76,8 +56,8 @@ export default function KakaoMapScript() {
       const lon = position.coords.longitude;
 
       const locPosition = new kakao.maps.LatLng(lat, lon);
-      const message = '<div style="padding:5px;">부이야</div>';
-
+      const message = '내 위치~';
+      console.log(lat, lon);
       displayMarker(locPosition, message);
     });
   } else {
